25e30747a6d3f6f58f4c75b64699edd6d8019634
EmIErO <mira.bryja@gmail.com>
2018-11-07 07:52:05 +0100

    Overload constructor, implement adding an element to dynamic array.

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,33 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 // put your code here!
 public class DynamicIntArray {
+
+    private int size;
+    private int[] data;
+
+    public DynamicIntArray() {
+        this.size = 0;
+        this.data = new int[20];
+    }
+
+    public DynamicIntArray(int initialCapacity) {
+        this.size = 0;
+        this.data = new int[initialCapacity];
+    }
+
+    public boolean add(int element) {
+        checkCapacity();
+        data[size++] = element;
+        return true;
+    }
+
+    private void checkCapacity() {
+        if ((size + 1) < data.length) {
+            int[] newData = Arrays.copyOf(data, data.length * 3 / 2);
+        }
+    }
 
 }
